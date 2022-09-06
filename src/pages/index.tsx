8382cf4892586dc96ec7d2ae67add9c4a89d26b8
EduhuSua <Eduardosuarez.ucv@gmail.com>
2022-09-05 21:58:55 -0400

    feat(Modal and button modal): creating componente button modal and modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
+import BaseModal from '@components/modals';
+
 import { SERVER_URLS } from '@config';
 
 const Landing: NextPage = () => {
   return (
     <>
       <div className="flex flex-col gap-y-4">
+        <BaseModal title="PRUEBA" buttonText="BOTON MODAL">
+          <p>PRUEBA DE MI PODEROSO Y CORPULEO MODAL</p>
+        </BaseModal>
         <Link href={SERVER_URLS.URL_LOGIN}>
           <a className="bg-primary hover:bg-blue-700 text-light font-bold py-2 px-4 cursor-pointer rounded-full w-full uppercase text-center transition">
             Iniciar Sesión
