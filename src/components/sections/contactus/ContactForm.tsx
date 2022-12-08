803647c375989f753981c482130333496cf413d0
Eduardo Suarez <eduardosuarez.c97@gmail.com>
2022-12-07 23:41:52 -0400

    done registro formulario de contacto

--- src/components/sections/contactus/ContactForm.tsx
+++ src/components/sections/contactus/ContactForm.tsx
@@ -8,12 +8,24 @@ import { getBusinessInfo, postContactUsInfo } from '@fetches/contact';
 
 import { FormikValues } from 'formik';
 import useSWR from 'swr';
+import * as Yup from 'yup';
 
 interface ContactUsForm {
   email: string;
   full_name: string;
   reason: string;
 }
+
+const ContactUsSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Correo invalido')
+    .required('Campo requerido')
+    .max(25, 'Cadena muy larga'),
+  full_name: Yup.string()
+    .required('Campo requerido')
+    .max(50, 'Cadena muy larga'),
+  reason: Yup.string().required('Campo requerido').max(150, 'Cadena muy larga'),
+});
 
 const ContactForm = ({ setLoading }: any) => {
   const { data } = useSWR('contact', () => getBusinessInfo());
@@ -38,55 +50,78 @@ const ContactForm = ({ setLoading }: any) => {
       <h2 className="font-bold text-lg text-center mb-4">
         Formulario de contacto
       </h2>
-      <Form initialValues={initValues} onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <div className="flex border-b border-gray py-2">
-            <label className="capitalize mr-2">para:</label>
-            <p className="px-2 border-none grow text-gray-600">{data?.email}</p>
-          </div>
-        </div>
-        <div className="mb-4">
-          <div className="flex border-b border-gray py-2">
-            <label htmlFor="email" className="capitalize mr-2">
-              de:
-            </label>
-            <Field
-              className="px-2 border-none grow text-gray-600"
-              name="email"
-              id="email"
-            />
-          </div>
-          <ErrorMsg name="email" />
-        </div>
-        <div className="mb-4">
-          <div className="flex border-b border-gray py-2">
-            <label htmlFor="full_name" className="capitalize mr-2">
-              nombre y apellido:
-            </label>
-            <Field
-              className="px-2 border-none grow"
-              name="full_name"
-              id="full_name"
-            />
-          </div>
-          <ErrorMsg name="full_name" />
-        </div>
-        <div className="mb-8">
-          <label htmlFor="reason" className="capitalize py-2 mb-2 block">
-            asunto:
-          </label>
-          <TextArea className="w-full h-56 resize-none" name="reason" />
-          <ErrorMsg name="reason" />
-        </div>
-        <div className="text-center">
-          <Button
-            bgColor="bg-darkprimary"
-            type="submit"
-            className="w-fit align-center"
-          >
-            Enviar
-          </Button>
-        </div>
+      <Form
+        validationSchema={ContactUsSchema}
+        initialValues={initValues}
+        onSubmit={handleSubmit}
+        renderProps
+      >
+        {({ errors, touched }) => (
+          <>
+            <div className="mb-4">
+              <div className="flex border-b border-gray py-2">
+                <label className="capitalize mr-2">para:</label>
+                <p className="px-2 border-none grow text-gray-600">
+                  {data?.email}
+                </p>
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="flex border-b border-gray py-2">
+                <label htmlFor="email" className="capitalize mr-2">
+                  de:
+                </label>
+                <Field
+                  className="px-2 border-none grow text-gray-600"
+                  name="email"
+                  id="email"
+                />
+              </div>
+              {errors.email && touched.email && (
+                <div className="bg-red-600 border border-red text-red w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
+                  <span>{errors.email}</span>
+                </div>
+              )}
+            </div>
+            <div className="mb-4">
+              <div className="flex border-b border-gray py-2">
+                <label htmlFor="full_name" className="capitalize mr-2">
+                  nombre y apellido:
+                </label>
+                <Field
+                  className="px-2 border-none grow"
+                  name="full_name"
+                  id="full_name"
+                />
+              </div>
+              {errors.full_name && touched.full_name && (
+                <div className="bg-red-600 border border-red text-red w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
+                  <span>{errors.full_name}</span>
+                </div>
+              )}
+            </div>
+            <div className="mb-8">
+              <label htmlFor="reason" className="capitalize py-2 mb-2 block">
+                asunto:
+              </label>
+              <TextArea className="w-full h-56 resize-none" name="reason" />
+            </div>
+            {errors.reason && touched.reason && (
+              <div className="bg-red-600 border border-red text-red w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
+                <span>{errors.reason}</span>
+              </div>
+            )}
+            <div className="text-center">
+              <Button
+                bgColor="bg-darkprimary"
+                type="submit"
+                className="w-fit align-center"
+              >
+                Enviar
+              </Button>
+            </div>
+          </>
+        )}
       </Form>
     </div>
   );
